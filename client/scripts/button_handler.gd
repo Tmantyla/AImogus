1333b63703590ebd0d2838655fb8b96c61ab7fc7
Teemu Mäntylä <teemu.mantyla05@gmail.com>
2025-11-15 21:11:23 +0200

    make client separate project

--- client/scripts/button_handler.gd
+++ client/scripts/button_handler.gd
@@ -9,20 +9,20 @@ func connect_buttons(root: Node):
 
 func _on_button_pressed(btn, category):
 	if category.name == "Stations":
-		_swap_scene("res://client/scenes/gameplay/station.tscn", btn.name)
+		_swap_scene("res://scenes/gameplay/station.tscn", btn.name)
 	if category.name == "Map":
-		_swap_scene("res://client/scenes/gameplay/map.tscn")
+		_swap_scene("res://scenes/gameplay/map.tscn")
 	if category.name == "Vending":
-		_swap_scene("res://client/scenes/gameplay/vending_machine.tscn", btn.name)
+		_swap_scene("res://scenes/gameplay/vending_machine.tscn", btn.name)
 	if category.name == "Back":
-		_swap_scene("res://client/scenes/gameplay/station.tscn", btn.name)
+		_swap_scene("res://scenes/gameplay/station.tscn", btn.name)
 	if category.name == "VendingMachineActions":
 		if btn.name == "Fix":
-			var scene := preload("res://client/scenes/ui/minigame_fix.tscn")
+			var scene := preload("res://scenes/ui/minigame_fix.tscn")
 			var inst := scene.instantiate()
 			add_child(inst)
 		if btn.name == "Restock":
-			var scene := preload("res://client/scenes/ui/minigame_restock.tscn")
+			var scene := preload("res://scenes/ui/minigame_restock.tscn")
 			var inst := scene.instantiate()
 			add_child(inst)
 
