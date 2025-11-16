43c96ba9eba3b1705d1eb3913f2dff4f0dad30aa
Teemu Mäntylä <teemu.mantyla05@gmail.com>
2025-11-16 04:11:42 +0200

    update map

--- client/scripts/scenes/map.gd
+++ client/scripts/scenes/map.gd
@@ -1,4 +1,6 @@
 extends Node2D
 
-func _ready():
-	ButtonHandler.connect_buttons(self)
+func _on__input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		PlayerState.station = self.name
+		get_tree().change_scene_to_file("res://scenes/gameplay/station.tscn")
